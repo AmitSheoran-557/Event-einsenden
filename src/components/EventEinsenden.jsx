997e1ad16f4170302f9db86fa0c3fb45918168a3
AmitSheoran-557 <amit22082006@gmail.com>
2025-01-03 15:22:50 +0530

    edit amx img width

--- src/components/EventEinsenden.jsx
+++ src/components/EventEinsenden.jsx
@@ -18,7 +18,15 @@ const EventEinsenden = () => {
     const handleImageSelect = (event) => {
         const files = event.target.files;
         const imageURLs = Array.from(files).map(file => URL.createObjectURL(file));
-        setImages(prevImages => [...prevImages, ...imageURLs]);
+
+        setImages((prevImages) => {
+            const totalImages = prevImages.length + imageURLs.length;
+            if (totalImages > 3) {
+                const remainingSlots = 3 - prevImages.length;
+                return [...prevImages, ...imageURLs.slice(0, remainingSlots)];
+            }
+            return [...prevImages, ...imageURLs];
+        });
     };
 
     return (
@@ -26,23 +34,24 @@ const EventEinsenden = () => {
             <div className="max-w-[1280px] mx-auto">
                 <h1 className='lg:mb-5 md:mb-4 mb-3 text-center mx-auto text-lightBlack font-bold lg:text-4xl md:text-3xl text-2xl !tracking-[-2%] !leading-[122%]'>Event einsenden</h1>
                 <p className='text-center lg:mb-5 mb-4 max-w-[768px] w-full text-primary font-light lg:text-xl md:text-base text-sm !leading-[150%]'>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
-               
-                <div className='max-w-[763px] flex overflow-x-auto w-full lg:mb-5 md:mb-4 mb-3 py-[7.2px] px-4 rounded-[30px] border-[0.5px] border-dashed bg-white'>
+
+                <div className='max-w-[763px] flex overflow-auto items-center w-full lg:mb-5 md:mb-4 mb-3 py-[7.2px] px-4 rounded-[30px] border-[0.5px] border-dashed bg-white'>
 
                     <div className='flex lg:gap-4 gap-3 items-center'>
-                        {images.map((image, index) => (
-                            <div key={index} className="lg:size-24 md:size-14 size-10 flex justify-center items-center lg:rounded-[24px] rounded-2xl overflow-hidden">
-                                <img src={image} alt={`Selected ${index}`} className="w-full h-full object-cover flex-wrap" />
-                            </div>
-                        ))}
-
+                        <div className='flex flex-wrap lg:gap-4 gap-3'>
+                            {images.map((image, index) => (
+                                <div key={index} className="lg:size-24 md:size-14 size-10 flex justify-center items-center lg:rounded-[24px] rounded-2xl overflow-hidden">
+                                    <img src={image} alt={`Selected ${index}`} className="w-full h-full object-cover flex-wrap" />
+                                </div>
+                            ))}
+                        </div>
                         <div className='lg:py-[30px] md:py-6 sm:py-5 py-4 lg:px-[38px] md:px-6 sm:px-5 px-4 border-[0.5px] border-dashed max-h-20 flex justify-center items-center rounded-[20px]'
                             onClick={() => document.getElementById('file-input').click()} >
                             <PlusIcon />
-                            <input id="file-input" type="file" accept="image/*" onChange={handleImageSelect} className="hidden" multiple />
+                            <input id="file-input" type="file" accept="image/*" disabled={images.length >= 3} onChange={handleImageSelect} className="hidden" multiple />
                         </div>
+
                     </div>
-                    
                 </div>
 
                 <form onSubmit={handleSubmit(onSubmit)} className='max-w-[763px] w-full mx-auto'>
